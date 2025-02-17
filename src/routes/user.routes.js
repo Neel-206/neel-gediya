e4207cc8b2aae34f86347a94c7ecd07a510bd04e
neel gediya <nkgediya206@gmail.com>
2025-02-17 22:20:45 +0530

    update README, enhance ApiResponse export, and add file upload support to user registration route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,16 @@ import { Router } from "express";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    UploadStream.fields([{
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]), registerUser
+);
 
 export default router
