86137f609e338c6d67d33e48de85e6a109501ab4
neel gediya <nkgediya206@gmail.com>
2025-02-17 21:38:39 +0530

    refactor app setup and add user registration route with async handler

--- src/app.js
+++ src/app.js
@@ -10,9 +10,15 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(express.static("public"));
 
+import userRouter from "./routes/user.routes.js";
 
+
+app.use("/api/v1/users", userRouter);
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promish.resolve(requestHandler(req, res, next)).
         catch((error) => next(error));
     }
