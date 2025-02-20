a57ad88e50cb84f7efe27ac3e7d563b3213cd99d
neel gediya <nkgediya206@gmail.com>
2025-02-20 12:11:07 +0530

    update MongoDB URI in .env; improve README formatting; adjust main entry in package.json; refactor connectDB function and its export; enhance server startup logging in index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,51 @@
-import dotenv from "dotenv";
-import { connectDB } from "./db/index.js";
-import { app } from "./app.js";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
-dotenv.config();
+
 
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 8000}`);
-        });
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        })
     })
-    .catch((error) => {
-        console.log("MONGODB connection FAILED ", error);
-    });
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
+
+
+
+
+
+
+
+
+
 
 /*
-import express from "express";
-const app = express();
-
-(async() => {
+import express from "express"
+const app = express()
+( async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${mydb}`)
-        app.on("error", (error) => {
-            console.log("Error:", error);
-            throw err;
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
         })
 
-        app.listen(process.env.port, () => {
-            console.log(`app is listening on port ${process.env.port}`);
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
         })
+
     } catch (error) {
-        console.error("ERROR:", error)
+        console.error("ERROR: ", error)
         throw err
     }
-})()*/
+})()
+
+*/
