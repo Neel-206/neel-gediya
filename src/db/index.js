fa013b8000ed1aa073d445c71c26b8fce6e6a3b8
neel gediya <nkgediya206@gmail.com>
2025-02-20 10:50:33 +0530

    export connectDB function for improved database connection handling; fix log message formatting

--- src/db/index.js
+++ src/db/index.js
@@ -2,16 +2,15 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
-const connectDB = async() => {
+export const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         if (connectionInstance) {
-            console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+            console.log(`\n MongoDB connected!!DB HOST: ${connectionInstance.connection.host}`);
         }
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
         process.exit(1)
+
     }
 }
-
-export { connectDB };
