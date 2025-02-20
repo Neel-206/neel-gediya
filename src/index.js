c21da74ddc297d32ad63a89b8db79f49cc287d65
neel gediya <nkgediya206@gmail.com>
2025-02-20 11:27:27 +0530

    update MongoDB connection URI; improve README formatting; clean up index.js and db connection logic

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
-//require('dotenv').config({ path: './env' });
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 import { app } from "./app.js";
 
-
-
-dotenv.config()
-
-
+dotenv.config();
 
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`servre is running on port ${process.env.PORT}`);
-        })
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        });
     })
     .catch((error) => {
         console.log("MONGODB connection FAILED ", error);
-    })
-
-
-
-
-
-
+    });
 
 /*
 import express from "express";
